525e6bf339a9c5ca1df5317a9a877d14a3b2e99a
A.J. Cabanatuan <allanjay808@ksu.edu>
2016-11-14 13:44:39 -0600

    test app class, and skeleton for Boss class

---
+++ src/app-boss-test.js
@@ -0,0 +1,77 @@
+"use strict";
+
+/* Classes and Libraries */
+const Game = require('./game');
+const Boss = require('.boss');
+
+/* Global variables */
+var canvas = document.getElementById('screen');
+var game = new Game(canvas, update, render);
+
+var background = new Image();
+background.src = 'assets/background.png';
+
+/**
+ * @function masterLoop
+ * Advances the game in sync with the refresh rate of the screen
+ * @param {DOMHighResTimeStamp} timestamp the current time
+ */
+var masterLoop = function(timestamp) {
+  game.loop(timestamp);
+  window.requestAnimationFrame(masterLoop);
+}
+masterLoop(performance.now());
+
+/**
+ * @function update
+ * Updates the game state, moving
+ * game objects and handling interactions
+ * between them.
+ * @param {DOMHighResTimeStamp} elapsedTime indicates
+ * the number of milliseconds passed since the last frame.
+ */
+function update(elapsedTime) {
+  // TODO: Update the Boss
+}
+
+/**
+  * @function render
+  * Renders the current game state into a back buffer.
+  * @param {DOMHighResTimeStamp} elapsedTime indicates
+  * the number of milliseconds passed since the last frame.
+  * @param {CanvasRenderingContext2D} ctx the context to render to
+  */
+function render(elapsedTime, ctx) {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 1024, 786);
+
+  // TODO: Render background
+
+  // Transform the coordinate system using
+  // the camera position BEFORE rendering
+  // objects in the world - that way they
+  // can be rendered in WORLD cooridnates
+  // but appear in SCREEN coordinates
+  renderWorld(elapsedTime, ctx);
+}
+
+/**
+  * @function renderWorld
+  * Renders the entities in the game world
+  * IN WORLD COORDINATES
+  * @param {DOMHighResTimeStamp} elapsedTime
+  * @param {CanvasRenderingContext2D} ctx the context to render to
+  */
+function renderWorld(elapsedTime, ctx) {
+    // Render the player
+	ctx.drawImage(
+		background,
+		0, 0, 640, 400,
+		0, 0, canvas.width, canvas.height);
+
+	ctx.save();
+
+  //TODO: Render the Boss
+
+	ctx.restore();
+}

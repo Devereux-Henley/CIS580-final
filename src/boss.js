58ef854e21507adf5b0e38ccb66b93d30e326294
Devereux Henley <devereux@ksu.edu>
2016-11-16 15:14:42 -0600

    just a skeleton, add values before testing

--- src/boss.js
+++ src/boss.js
@@ -10,8 +10,9 @@ module.exports = exports = Boss;
   * @constructor Boss
   * Creates a Boss
   */
-function Boss() {
-
+function Boss(spd) {
+  this.state = "idle";
+  this.speed = spd;
 }
 
 /**
@@ -20,6 +21,50 @@ function Boss() {
   * @param {DOMHighResTimeStamp} elapedTime
   */
 Boss.prototype.update = function(elapsedTime) {
+  var isModSpd = (elapsedTime % this.speed == 0);
+  var timer;
+  if(state == "idle"){
+    timer = 0;
+    if(isModSpd){
+      timer++;
+    }
+    if(timer == 2*this.speed){
+      state = "attack";
+    }
+  }
+  if(state == "attack"){
+    Attack();
+    state = "wait to move";
+  }
+  if(state == "wait to move"){
+    timer = 0;
+    if(isModSpd){
+      timer++;
+    }
+    if(timer == spd){
+      state = "move";
+    }
+  }
+  if(state == "move"){
+    Move();
+    state = "wait to attack";
+  }
+  if(state == "wait to attack"){
+    timer = 0;
+    if(isModSpd){
+      timer++;
+    }
+    if(timer == spd){
+      state = "attack";
+    }
+  }
+}
+
+Attack(){
+
+}
+
+Move(){
 
 }
 
@@ -30,5 +75,15 @@ Boss.prototype.update = function(elapsedTime) {
  * @param {CanvasRenderingContext2D} ctx
  */
 Boss.prototype.render = function (elapsedTime, ctx) {
-
+ctx.clear();
+  if(state == "attack"){
+    ctx.style = "red";
+  }
+  else if(state == "move"){
+    ctx.style = "blue";
+  }
+  else{
+    ctx.style = "purple";
+  }
+  ctx.drawRect();
 }

33a6ccb625f5b99917e9b7975a1bed40bcfe4075
Devereux Henley <devereux@ksu.edu>
2016-11-16 15:27:24 -0600

    HMMMMMMMMMMM CAPITAL BUGS

--- src/app.js
+++ src/app.js
@@ -27,31 +27,32 @@ background.src = 'assets/background.png';
  * Handles keydown events
  */
 window.onkeydown = function(event) {
+  input.shift = event.shiftKey;
   switch(event.key) {
+	case "W":
     case "ArrowUp":
     case "w":
       input.up = true;
       event.preventDefault();
       break;
+	case "S":
     case "ArrowDown":
     case "s":
       input.down = true;
       event.preventDefault();
       break;
+	case "A":
     case "ArrowLeft":
     case "a":
       input.left = true;
       event.preventDefault();
       break;
+	case "D":
     case "ArrowRight":
     case "d":
       input.right = true;
 	  event.preventDefault();
-      break;
-	case "Shift":
-	  input.shift = true;
-	  event.preventDefault();
-	  break;
+      break;	
 	case " ":
 	  input.space = true;
 	  event.preventDefault();
@@ -64,31 +65,32 @@ window.onkeydown = function(event) {
  * Handles keydown events
  */
 window.onkeyup = function(event) {
+  input.shift = event.shiftKey;
   switch(event.key) {
+	case "W":
     case "ArrowUp":
     case "w":
       input.up = false;
       event.preventDefault();
       break;
+	case "S":
     case "ArrowDown":
     case "s":
       input.down = false;
       event.preventDefault();
       break;
+	case "A":
     case "ArrowLeft":
     case "a":
       input.left = false;
       event.preventDefault();
       break;
+	case "D":
     case "ArrowRight":
     case "d":
       input.right = false;
       event.preventDefault();
       break;
-	case "Shift": 
-	  input.shift = false;
-	  event.preventDefault();
-	  break;
 	case " ":
 	  input.space = false;
 	  event.preventDefault();

d511ad799952a3067b0945fa8fde81af344e9cdc
Gunnarr07 <jeremyrt88@gmail.com>
2016-11-30 14:54:57 -0600

    work on collisions

--- src/collision.js
+++ src/collision.js
@@ -10,10 +10,10 @@ module.exports = exports = Collision;
 
 Collision.prototype.checkForSingleSquareCollision = function(entity1, entity2)
  {
-  var collides = !(entity1.x + entity1.width < entity2.x ||
-                   entity1.x > entity2.x + entity2.width ||
-                   entity1.y + entity1.height < entity2.y ||
-                   entity1.y > entity2.y + entity2.height);
+  var collides = !(entity1.points[0].x + entity1.width < entity2.points[0].x ||
+                   entity1.points[0].x > entity2.points[0].x + entity2.width ||
+                   entity1.points[0].y + entity1.height < entity2.points[0].y ||
+                   entity1.points[0].y > entity2.points[0].y + entity2.height);
   if(collides) {
     return true;
   }
@@ -22,8 +22,8 @@ Collision.prototype.checkForSingleSquareCollision = function(entity1, entity2)
 Collision.prototype.checkForSingleCircleCollision = function(entity1, entity2)
 {
     var distSquared =
-      Math.pow(entity1.position.x - entity2.position.x, 2) +
-      Math.pow(entity1.position.y - entity2.position.y, 2);
+      Math.pow(entity1.points[0].x - entity2.points[0].x, 2) +
+      Math.pow(entity1.points[0].y - entity2.points[0].y, 2);
     // (15 + 15)^2 = 900 -> sum of two balls' raidius squared
     var sumRadiusSqueared = (entity1.radius * entity1.radius) + (entity2.radius * entity2.radius);
     if(distSquared < sumRadiusSqueared)

fc57a65f9263748c21bbbfa52d351939b966edcc
Aaron Schif <aschif@netdevgroup.com>
2016-12-12 22:27:17 -0600

    Add gui

---
+++ src/gui.js
@@ -0,0 +1,56 @@
+// @flow
+
+const Player = require('./player');
+
+const img = new Image();
+img.src = 'assets/heart_full.png';
+const HEART_PADDING = 2;
+const HEART_SIZE = 24;
+const HEART_NUM = 3;
+const HEART_IMG = createHeartImage();
+
+class Gui {
+    /*::
+    player: Player
+    */
+    constructor(player/*: Player */) {
+        this.player = player;
+    }
+
+    render(
+        dt/*: number */,
+        ctx/*: CanvasRenderingContext2D */
+    ) {
+        let h = this.player.health;
+        let p = 6;
+        let max_width = HEART_IMG.width;
+        let o = max_width - (HEART_PADDING * (h - 1) + HEART_SIZE * h) / 2;
+        let x = ctx.canvas.width - HEART_IMG.width;
+        ctx.drawImage(
+            HEART_IMG,
+            o, 0, max_width, HEART_SIZE,
+            x - p + o, p, max_width, HEART_SIZE
+        );
+    }
+}
+
+module.exports.Gui = Gui;
+
+
+function createHeartImage() {
+    const canvas = document.createElement("canvas");
+    canvas.width = HEART_PADDING * (HEART_NUM - 1) + HEART_SIZE * HEART_NUM;
+    canvas.height = HEART_SIZE;
+    let ctx = canvas.getContext('2d');
+    if (!ctx) throw "";
+
+    let x = -HEART_PADDING;
+    for (let i=0; i<HEART_NUM; i++) {
+        ctx.drawImage(
+            img,
+            0, 0, img.width, img.height,
+            (i*(HEART_PADDING+HEART_SIZE)), 0, HEART_SIZE, HEART_SIZE,
+        );
+    }
+    return canvas;
+}
--- src/level_creepy_crawler/level.js
+++ src/level_creepy_crawler/level.js
@@ -4,16 +4,20 @@ const {Level: AbstractLevel} = require("../level_chooser/main");
 const {Map} = require("../map");
 const Player = require("../player");
 const mapdata = require('./tileMap');
+const {Gui} = require('../gui');
+
 
 class Level extends AbstractLevel {
     /*::
     player: Player
     map: Map
+    gui: Gui
     */
     constructor() {
         super();
         this.player = new Player({x: 500, y: 500});
         this.map = new Map(2, mapdata);
+        this.gui = new Gui(this.player);
     }
 
     render(
@@ -26,6 +30,7 @@ class Level extends AbstractLevel {
             layer.render(ctx);
         }
         this.player.render(dt, ctx);
+        this.gui.render(dt, ctx);
     }
 
     update(

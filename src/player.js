0061c5ef6217010f49d671bde253471810699c1e
cavossman <cvossman@ksu.edu>
2016-12-15 17:41:15 -0600

    Added the improved movement from Mr D.

--- src/player.js
+++ src/player.js
@@ -2,76 +2,79 @@
 
 const input = require('./inputHandler').inputState;
 
-
 /* Constants */
-const PLAYER_SPEED = 2;
+const PLAYER_SPEED = 2.0;
+const DODGE_SPEED = 1.5 * PLAYER_SPEED;
+const SPRINT_SPEED = 2.0 * PLAYER_SPEED;
 const RENDER_TIMER = 200;
+const STAMINA_DECAY = 2;
+const STAMINA_RECHARGE = 1;
 const DODGE_END = 4 * RENDER_TIMER;
 const DODGE_DELAY = 10 * RENDER_TIMER;
 
-const EAST_WALK =
-[new SheetPosition(0, 96),
- new SheetPosition(24, 96),
- new SheetPosition(48, 96),
- new SheetPosition(72, 96),
- new SheetPosition(96, 96),
- new SheetPosition(120, 96),
- new SheetPosition(144, 96),
- new SheetPosition(168, 96)];
+const EAST_WALK = [new SheetPosition(0, 96),
+    new SheetPosition(24, 96),
+    new SheetPosition(48, 96),
+    new SheetPosition(72, 96),
+    new SheetPosition(96, 96),
+    new SheetPosition(120, 96),
+    new SheetPosition(144, 96),
+    new SheetPosition(168, 96)
+];
 
- const NORTH_WALK =
- [new SheetPosition(0, 32),
-	new SheetPosition(24, 32),
-	new SheetPosition(48, 32),
-	new SheetPosition(72, 32),
-	new SheetPosition(96, 32),
-	new SheetPosition(120, 32),
-	new SheetPosition(144, 32),
-	new SheetPosition(168, 32)];
+const NORTH_WALK = [new SheetPosition(0, 32),
+    new SheetPosition(24, 32),
+    new SheetPosition(48, 32),
+    new SheetPosition(72, 32),
+    new SheetPosition(96, 32),
+    new SheetPosition(120, 32),
+    new SheetPosition(144, 32),
+    new SheetPosition(168, 32)
+];
 
- const WEST_WALK =
- [new SheetPosition(0, 64),
-	new SheetPosition(24, 64),
-	new SheetPosition(48, 64),
-	new SheetPosition(72, 64),
-	new SheetPosition(96, 64),
-	new SheetPosition(120, 64),
-	new SheetPosition(144, 64),
-	new SheetPosition(168, 64)];
+const WEST_WALK = [new SheetPosition(0, 64),
+    new SheetPosition(24, 64),
+    new SheetPosition(48, 64),
+    new SheetPosition(72, 64),
+    new SheetPosition(96, 64),
+    new SheetPosition(120, 64),
+    new SheetPosition(144, 64),
+    new SheetPosition(168, 64)
+];
 
- const SOUTH_WALK=
- [new SheetPosition(0, 0),
-	new SheetPosition(24, 0),
-	new SheetPosition(48, 0),
-	new SheetPosition(72, 0),
-	new SheetPosition(96, 0),
-	new SheetPosition(120, 0),
-	new SheetPosition(144, 0),
-	new SheetPosition(168, 0)];
+const SOUTH_WALK = [new SheetPosition(0, 0),
+    new SheetPosition(24, 0),
+    new SheetPosition(48, 0),
+    new SheetPosition(72, 0),
+    new SheetPosition(96, 0),
+    new SheetPosition(120, 0),
+    new SheetPosition(144, 0),
+    new SheetPosition(168, 0)
+];
 
-	const EAST_DODGE=
-	[new SheetPosition(0, 96),
-	 new SheetPosition(0, 0),
-	 new SheetPosition(0, 64),
-	 new SheetPosition(0, 32)];
+const EAST_DODGE = [new SheetPosition(0, 96),
+    new SheetPosition(0, 0),
+    new SheetPosition(0, 64),
+    new SheetPosition(0, 32)
+];
 
-	const NORTH_DODGE=
-	[new SheetPosition(0, 32),
-	 new SheetPosition(0, 96),
-	 new SheetPosition(0, 0),
-	 new SheetPosition(0, 64)];
+const NORTH_DODGE = [new SheetPosition(0, 32),
+    new SheetPosition(0, 96),
+    new SheetPosition(0, 0),
+    new SheetPosition(0, 64)
+];
 
-	const WEST_DODGE=
-	[new SheetPosition(0, 64),
-	 new SheetPosition(0, 96),
-   new SheetPosition(0, 32),
-   new SheetPosition(0, 0)];
+const WEST_DODGE = [new SheetPosition(0, 64),
+    new SheetPosition(0, 96),
+    new SheetPosition(0, 32),
+    new SheetPosition(0, 0)
+];
 
-	const SOUTH_DODGE=
-	[new SheetPosition(0, 0),
-	 new SheetPosition(0, 64),
-	 new SheetPosition(0, 32),
-	 new SheetPosition(0, 96)];
+const SOUTH_DODGE = [new SheetPosition(0, 0),
+    new SheetPosition(0, 64),
+    new SheetPosition(0, 32),
+    new SheetPosition(0, 96)
+];
 
 /**
  * @module Player
@@ -80,21 +83,21 @@ const EAST_WALK =
 module.exports = exports = Player;
 
 function PlayerState(initDirection, ty) {
-	this.moveState = initDirection;
-	this.moveType = ty;
+    this.moveState = initDirection;
+    this.moveType = ty;
 }
 
 function SheetPosition(x, y) {
-	this.x = x;
-	this.y = y;
-	this.width = 24;
-	this.height = 32;
+    this.x = x;
+    this.y = y;
+    this.width = 24;
+    this.height = 32;
 }
 
 function MoveStyleRenders(normal, sprinting, dodging) {
-	this.NORMAL = normal;
-	this.SPRINTING = sprinting;
-	this.DODGING = dodging;
+    this.NORMAL = normal;
+    this.SPRINTING = sprinting;
+    this.DODGING = dodging;
 }
 
 /**
@@ -109,7 +112,7 @@ function Player(position) {
 	this.renderSource = new Image();
 	this.renderSource.src = 'assets/rpg_sprite_walk.png';
 	this.timer = 0;
-	this.dodgeTimer = 0;
+  this.dodgeTimer = 0;
 	this.renderPosition = 0;
 	this.renderSources =
 		{'STILL':
@@ -198,60 +201,65 @@ function Player(position) {
 }
 
 Player.prototype.walk = function() {
-	this.state.moveType = 'NORMAL';
+    if (this.dodgeTimer > 0 && this.dodgeTimer < DODGE_END) return;
+    this.state.moveType = 'NORMAL';
 }
 
 Player.prototype.sprint = function() {
-	if(this.state.moveType == 'DODGING') return;
-	this.state.moveType = 'SPRINTING';
+    if (this.state.moveType == 'DODGING') return;
+    this.state.moveType = 'SPRINTING';
 }
 
 Player.prototype.dodge = function() {
-	this.state.moveType = 'DODGING';
+    if (DODGE_END < this.dodgeTimer && this.dodgeTimer < DODGE_DELAY) {
+        this.walk();
+    } else {
+        this.state.moveType = 'DODGING';
+    }
 }
 
 Player.prototype.moveWest = function() {
-	this.state.moveState = 'WEST';
+    this.state.moveState = 'WEST';
 }
 
 Player.prototype.moveEast = function() {
-	this.state.moveState = 'EAST';
+    this.state.moveState = 'EAST';
 }
 
 Player.prototype.moveNorth = function() {
-	this.state.moveState = 'NORTH';
+    this.state.moveState = 'NORTH';
 }
 
 Player.prototype.moveSouth = function() {
-	this.state.moveState = 'SOUTH';
+    this.state.moveState = 'SOUTH';
 }
 
 Player.prototype.moveNorthEast = function() {
-	this.state.moveState = 'NORTHEAST';
+    this.state.moveState = 'NORTHEAST';
 }
 
 Player.prototype.moveNorthWest = function() {
-	this.state.moveState = 'NORTHWEST';
+    this.state.moveState = 'NORTHWEST';
 }
 
 Player.prototype.moveSouthEast = function() {
-	this.state.moveState = 'SOUTHEAST';
+    this.state.moveState = 'SOUTHEAST';
 }
 
 Player.prototype.moveSouthWest = function() {
-	this.state.moveState = 'SOUTHWEST';
+    this.state.moveState = 'SOUTHWEST';
 }
 
 Player.prototype.still = function() {
-	this.state.moveState = 'STILL';
+    this.state.moveState = 'STILL';
 }
 
 Player.prototype.damage = function() {
-	this.health--;
+    this.health--;
 }
 
 Player.prototype.getHealth = function() {
-	return this.health;
+    return this.health;
 }
 
 // Handle Collisions
@@ -279,7 +287,6 @@ Player.prototype.onCollision = function(entity) {
 		}
 	}
 
-
 /**
  * @function update
  * Updates the player based on the supplied input
@@ -290,66 +297,65 @@ Player.prototype.onCollision = function(entity) {
 Player.prototype.update = function(elapsedTime) {
     this.checkMoveState();
 
-	// set the velocity
-	this.velocity.x = 0;
-	this.velocity.y = 0;
+    // set the velocity
+    this.velocity.x = 0;
+    this.velocity.y = 0;
 
-	var updateSpeed = PLAYER_SPEED;
+    var updateSpeed = PLAYER_SPEED;
 
-	if(this.state.moveType == 'DODGING') {
-		updateSpeed = 1.5 * PLAYER_SPEED;
-	}
-	else if(this.state.moveType == 'SPRINTING') {
-		updateSpeed = 2 * PLAYER_SPEED;
-	}
-
-	switch(this.state.moveState) {
-		case 'NORTH':
-			this.velocity.y -= updateSpeed;
-			break;
-		case 'EAST':
-			this.velocity.x += updateSpeed;
-			break;
-		case 'SOUTH':
-			this.velocity.y += updateSpeed;
-			break;
-		case 'WEST':
-			this.velocity.x -= updateSpeed;
-			break;
-		case 'NORTHEAST':
-			this.velocity.x += updateSpeed;
-			this.velocity.y -= updateSpeed;
-			break;
-		case 'NORTHWEST':
-			this.velocity.x -= updateSpeed;
-			this.velocity.y -= updateSpeed;
-			break;
-		case 'SOUTHEAST':
-			this.velocity.x += updateSpeed;
-			this.velocity.y += updateSpeed;
-			break;
-		case 'SOUTHWEST':
-			this.velocity.x -= updateSpeed;
-			this.velocity.y += updateSpeed;
-			break;
-	}
-	
-	// Stamina
-	if (this.state.moveType == 'DODGING') {
-        updateSpeed = 1.5 * PLAYER_SPEED;
+    console.log(this.stamina);
+    if (this.state.moveType == 'DODGING') {
+        updateSpeed = DODGE_SPEED;
         this.dodgeTimer += elapsedTime;
     } else if (this.state.moveType == 'SPRINTING') {
-        if (this.stamina >= 2) {
-          updateSpeed = 2 * PLAYER_SPEED;
-          this.stamina -= 2;
+        if (this.stamina >= STAMINA_DECAY) {
+          updateSpeed = SPRINT_SPEED;
+          this.stamina -= STAMINA_DECAY;
         } else {
           this.walk();
         }
     } else if (this.stamina < 100) {
-      this.stamina += 1;
-    }   
-	
-	if (this.dodgeTimer > DODGE_END) {
+      this.stamina += STAMINA_RECHARGE;
+    }
+
+    switch (this.state.moveState) {
+        case 'NORTH':
+            this.velocity.y -= updateSpeed;
+            break;
+        case 'EAST':
+            this.velocity.x += updateSpeed;
+            break;
+        case 'SOUTH':
+            this.velocity.y += updateSpeed;
+            break;
+        case 'WEST':
+            this.velocity.x -= updateSpeed;
+            break;
+        case 'NORTHEAST':
+            this.velocity.x += updateSpeed;
+            this.velocity.y -= updateSpeed;
+            break;
+        case 'NORTHWEST':
+            this.velocity.x -= updateSpeed;
+            this.velocity.y -= updateSpeed;
+            break;
+        case 'SOUTHEAST':
+            this.velocity.x += updateSpeed;
+            this.velocity.y += updateSpeed;
+            break;
+        case 'SOUTHWEST':
+            this.velocity.x -= updateSpeed;
+            this.velocity.y += updateSpeed;
+            break;
+    }
+
+    // move the player
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.timer += elapsedTime;
+
+    if (this.dodgeTimer > DODGE_END) {
         this.walk();
         this.dodgeTimer += elapsedTime;
     }
@@ -357,29 +363,24 @@ Player.prototype.update = function(elapsedTime) {
     if (this.dodgeTimer > DODGE_DELAY) {
         this.dodgeTimer = 0;
     }
-	// End stamina
-	
-	
-	// move the player
-	this.position.x += this.velocity.x;
-	this.position.y += this.velocity.y;
 
-  this.timer += elapsedTime;
+    if (this.state.moveType == 'DODGING') {
 
-  if(this.timer > RENDER_TIMER) {
-    this.renderPosition++;
-    this.timer = 0;
-  }
+    }
 
-  var nextRender = this.renderSources[this.state.moveState][this.state.moveType][this.renderPosition];
+    if (this.timer > RENDER_TIMER) {
+        this.renderPosition++;
+        this.timer = 0;
+    }
 
-  if(nextRender == undefined) {
-    this.renderPosition = 0;
-    nextRender = this.renderSources[this.state.moveState][this.state.moveType][this.renderPosition];
-  }
-  else {
-    this.currentRender = nextRender;
-  }
+    var nextRender = this.renderSources[this.state.moveState][this.state.moveType][this.renderPosition];
+
+    if (nextRender == undefined) {
+        this.renderPosition = 0;
+        nextRender = this.renderSources[this.state.moveState][this.state.moveType][this.renderPosition];
+    } else {
+        this.currentRender = nextRender;
+    }
 }
 
 /**
@@ -389,15 +390,15 @@ Player.prototype.update = function(elapsedTime) {
  * @param {CanvasRenderingContext2D} ctx
  */
 Player.prototype.render = function(elapsedTime, ctx) {
-	ctx.save();
-	ctx.translate(this.position.x, this.position.y);
+    ctx.save();
+    ctx.translate(this.position.x, this.position.y);
 
     //Select rendersheet based on time passed since starting this state.
-	ctx.drawImage(
-		this.renderSource,
-		this.currentRender.x, this.currentRender.y, this.currentRender.width, this.currentRender.height,
-		0, 0, 24, 32);
-	ctx.restore();
+    ctx.drawImage(
+        this.renderSource,
+        this.currentRender.x, this.currentRender.y, this.currentRender.width, this.currentRender.height,
+        0, 0, 24, 32);
+    ctx.restore();
 }
 
 

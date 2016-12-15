fd94f8004b5736ba06fc6a08d478023c4c38c878
A.J. Cabanatuan <allanjay808@ksu.edu>
2016-12-15 10:21:40 -0600

    modified boss map, adding tile skeletion

---
+++ src/tile.js
@@ -0,0 +1,27 @@
+"use strict;"
+
+/**
+ * @module Tile
+ * A class representing a tile map object
+ */
+module.exports = exports = Tile;
+
+/**
+ * @constructor Tile
+ * @param {Position} position Where tile will be located on map
+ */
+function Tile(position) {
+  this.tag = "Tile";
+}
+
+Tile.prototype.update = function() {
+
+}
+
+Tile.prototype.render = function() {
+
+}
+
+Tile.prototype.onCollision = function() {
+
+}
